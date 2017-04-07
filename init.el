5b539f68317e79f17f85c23dc0e44e4bb0d0fe77
wyy <wyyjtu@gmail.com>
2017-04-08 00:25:20 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,23 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (go-mode zenburn-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(unless package-archive-contents
+  (package-refresh-contents)
+  (package-install-selected-packages))
+
+(load-theme 'zenburn t)
